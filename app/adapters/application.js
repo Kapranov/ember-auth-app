248b0e86707674c03641ce3b549c633c7e5f9fa0
Oleg G.Kapranov <oleg@vertiopia.com>
2016-09-03 21:59:45 +0300

    the access token is included in the XHR request headers vs application route /login

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,15 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-  namespace: 'api'
+  namespace: 'api',
+
+  authManager: Ember.inject.service(),
+
+  headers: Ember.computed('authManager.accessToken', function() {
+    return {
+      "Authorization": `Bearer ${this.get("authManager.accessToken")}`
+    };
+  })
+
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
   // model() {
   //   return this.store.findAll('code');
   // }
+
+  actions: {
+    error: function() {
+      this.transitionTo('/login');
+      return false;
+    }
+  }
+
 });
