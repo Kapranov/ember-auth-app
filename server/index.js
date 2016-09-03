a279f7a9695d6be8f6a61d806a340ea15ad4446d
Oleg G.Kapranov <oleg@vertiopia.com>
2016-09-03 10:19:01 +0300

    protect data at the source req.headers

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ module.exports = function(app) {
   app.use(bodyParser.urlencoded({ extended: true }));
 
   app.get('/api/codes', function (req, res) {
+
+    if (req.headers['authorization'] !== "holy moly these staff") {
+      return res.status(401).send('Unauthorized');
+    }
+
     return res.status(200).send({
       codes: [
         { id: 1, description: 'Obama Nuclear Missile Launching Code is: lovedronesandthensa' },
