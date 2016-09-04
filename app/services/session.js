7895f6ed158ac1de4cecd7b0443dd3f853607a53
Oleg G.Kapranov <oleg@vertiopia.com>
2016-09-04 12:38:37 +0300

    added /api/users

--- app/services/session.js
+++ app/services/session.js
@@ -1,4 +1,18 @@
-import Ember from 'ember';
+// import Ember from 'ember';
+import DS from 'ember-data';
+import ESASession from "ember-simple-auth/services/session";
 
-export default Ember.Service.extend({
+export default ESASession.extend({
+
+  store: Ember.inject.service(),
+
+  currentUser: Ember.computed('isAuthenticated', function() {
+    if (this.get('isAuthenticated')) {
+      const promise = this.get('store').queryRecord('user', {})
+      return DS.PromiseObject.create({ promise: promise })
+    }
+  })
 });
+
+// export default Ember.Service.extend({
+// });
