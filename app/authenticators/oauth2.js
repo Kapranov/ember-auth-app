9e7643e4b75e06310df1bd113a462829582b1993
Oleg G.Kapranov <oleg@vertiopia.com>
2016-09-04 20:59:50 +0300

    genereted authenticators vs oauth2.js

---
+++ app/authenticators/oauth2.js
@@ -0,0 +1,8 @@
+import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
+
+// If you needed to override the token endpoint, here’s how:
+// export default OAuth2PasswordGrant.extend({
+//   serverTokenEndpoint: "/path/to/token"
+// });
+
+export default OAuth2PasswordGrant.extend();
